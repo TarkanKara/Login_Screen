cdaedfa1982ef7012ed1cc9104fb54094444b09e
Tarkan Kara <unnahmet23@hotmail.com>
2022-12-25 15:44:16 +0300

    Updated

--- lib/utils/show_alert.dart
+++ lib/utils/show_alert.dart
@@ -1,5 +1,10 @@
+import 'package:flutterfire_login_ui/utils/constant.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 void showAlert(String msg) {
-  Fluttertoast.showToast(msg: msg);
+  Fluttertoast.showToast(
+    msg: msg,
+    fontSize: 15,
+    backgroundColor: kSecondColor,
+  );
 }
